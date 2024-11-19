70d3094d249a4523fb55d509b4841c125af823ac
yyko <68428103+oexlkinq@users.noreply.github.com>
2024-11-19 13:05:03 +0500

    метод для запросов к апи изменён на get

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,3 +1,3 @@
 import { SchApi } from "node-sch-api";
 
-export const api = new SchApi(import.meta.env.VITE_CUSTOM_SCH_API ?? (window.location.origin + '/sch_api/index.php'))
+export const api = new SchApi(import.meta.env.VITE_CUSTOM_SCH_API ?? (window.location.origin + '/sch_api/index.php'), 'get')
